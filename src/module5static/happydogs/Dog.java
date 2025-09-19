fecf5d2306b12ad1dbb1bf5ca5f87b3fbc827181
peremann <per.lauvas@kristiania.no>
2025-09-19 09:05:17 +0200

    Dog example with comments

--- src/module5static/happydogs/Dog.java
+++ src/module5static/happydogs/Dog.java
@@ -2,16 +2,26 @@ package module5static.happydogs;
 
 public class Dog {
 
+    // Three different types of attributes (for variation)
     private String name;
     private boolean big;
     private int age;
 
+    /**
+     * As I only have 1 constructor in this class (with three parameters - name, big and age),
+     * the only way to create a dog object is to provide values for all three parameters.
+     */
     public Dog(String name, boolean big, int age) {
+        // This is where I set the state for the new object to be made.
         this.name = name;
         this.big = big;
         this.age = age;
     }
 
+    /**
+     * Example of a method with no parameters (input)
+     * and not returning anything to the caller (void)
+     */
     public void presentYourself(){
         System.out.println("I am a dog, and my name is:"+name);
         System.out.println("I am " + age + " years old");
@@ -22,6 +32,11 @@ public class Dog {
         }
     }
 
+    /**
+     * Another example of a method with no parameters (input)
+     * and not returning anything to the caller (void).
+     * See how the value of attributes in the object may affect how the method behaves.
+     */
     public void saySometing(){
         if(big){
             System.out.println("VOFF VOFF!");
@@ -30,6 +45,12 @@ public class Dog {
         }
     }
 
+    /**
+     * Example of a method returning something.
+     * In this case a text (String) with information regarding the object state
+     * (the values of the objects attributes).
+     * We will later learn that the normal thing to use is a method called toString for this.
+     */
     public String getInfo(){
         String textToReturn = "I am a dog, and my name is:"+name + "\n" +
                 "I am " + age + " years old" + "\n";
@@ -41,6 +62,12 @@ public class Dog {
         return textToReturn;
     }
 
+    /**
+     * Per's favourite method (in this class).
+     * Why? It shows have a method can use both method input (the parameter "food")
+     * and its attributes (name, big, age) to do what it is supposed to do.
+     * Methods are about doing something. Some kind of action to be taken.
+     */
     public void eat (String food){
         System.out.println("Yummy, that was good!");
         System.out.println("As sure as my name is " + name + ", I LOVE " + food);
--- src/module5static/happydogs/Main.java
+++ src/module5static/happydogs/Main.java
@@ -1,20 +1,31 @@
 package module5static.happydogs;
 
 public class Main {
-    // TODO: How can we test the Dog class?
+    // How can we test the Dog class? We need a method. Let's use main.
 
-    // TODO: Test the different methods
+    // Testing the different methods we created in Dog using main.
     public static void main(String[] args) {
+        // We create two objects. They will have different states as we provide different values for them.
+        // We have to use -String boolean int- in that order, as it is what the constructor will accept.
         Dog d1 = new Dog("Fido", true, 4);
         Dog d2 = new Dog("Passop", false, 14);
+        // Now that the objects are created (and we have references to them; d1 and d2),
+        // we can test the methods in both objects.
+        // Se how I use SOUT to better understand the output displayed in the console.
+        System.out.println("Letting the objects present themselves:");
         d1.presentYourself();
         d2.presentYourself();
+
+        System.out.println("Letting the objects say something:");
         d1.saySometing();
         d2.saySometing();
-        System.out.println("Her kommer svar fra getInfo:");
-        String s1 = d1.getInfo();
+
+        // Now, I am calling a method that returns something - a String.
+        // I need to place the returned text somewhere. Aha - a String variable!
+        System.out.println("Here is what is returned from getInfo():");
+        String s1 = d1.getInfo(); // See how the returned text from the method is stored in variable s1
         System.out.println(s1);
-        String s2 = d2.getInfo();
+        String s2 = d2.getInfo(); // and s2
         System.out.println(s2);
 
         System.out.println("Providing food..");
