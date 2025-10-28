e43a10fe38a26f6646432bfd03c4783fbdd12eb7
peremann <per.lauvas@kristiania.no>
2025-10-28 07:59:04 +0100

    Module 11 Some describing comments

--- src/module11AbstractInterace/interfaceExample/Main.java
+++ src/module11AbstractInterace/interfaceExample/Main.java
@@ -1,6 +1,12 @@
 package module11AbstractInterace.interfaceExample;
 public class Main {
     public static void main(String[] args) {
+        /*
+        This is the main point.
+        Look how easy it is to inject different implementations of the interface.
+        The program can be run i two different modes. We simply plug in the
+        mode we want by choosing the implementation of the interface.
+         */
         Program p = new Program(new AnimalTestProvider());
         //Program p = new Program(new AnimaUserProvider());
         p.runProgram();
--- src/module11AbstractInterace/interfaceExample/Program.java
+++ src/module11AbstractInterace/interfaceExample/Program.java
@@ -3,12 +3,22 @@ package module11AbstractInterace.interfaceExample;
 import java.util.List;
 
 public class Program {
+    /*
+    Look! We have a dependency to an interface, and not a concrete class.
+    This is a looser coupling than a dependency to a concrete class.
+     */
     private final AnimalProvider animalProvider;
     public Program(AnimalProvider animalProvider) {
         this.animalProvider = animalProvider;
     }
 
     public void runProgram(){
+        /*
+        This is what we need the animal provider for. We need a list of animals.
+        We do not care about the implementation details. Just give me some animals.
+        Also notice that we use the List (interface) and not the implementation (i.e ArrayList).
+        We do not care about the details. We only need what is promised by the List interface.
+         */
         List<Animal> animals = animalProvider.getAnimals();
         System.out.println("Here are all the " + animals.size() + " animals:");
         for (Animal animal : animals) {
